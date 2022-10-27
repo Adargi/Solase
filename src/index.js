a0e6d1b44bed73431382abf631c631a648198116
Adargi <danielmorgonhart@outlook.com>
2022-10-27 18:39:46 +0100

    Branch Update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const { Client, IntentsBitField } = require('discord.js')
+const CH = require('command-handler')
+const path = require('path')
 require('dotenv/config')
 
 const client = new Client({
@@ -7,7 +9,12 @@ const client = new Client({
 
 client.on('ready', () => {
    console.log('The bot is ready')
+
+   new CH({
+      client,
+      commandsDir: path.join(__dirname, 'commands'),
+   })
 })
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
 

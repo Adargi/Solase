5ca1ad4f8305c9ab3267bd26e703aa25b9376ba4
Adargi <danielmorgonhart@outlook.com>
2022-10-27 14:47:58 +0100

    restore

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { Client, IntentsBitField } = require('discord.js')
 require('dotenv/config')
 
+const client = new Client({
+   intents: [IntentsBitField.Flags.Guilds],
+})
+
 client.on('ready', () => {
    console.log('The bot is ready')
 })
